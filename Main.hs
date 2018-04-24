373bb1c20ee15c50597528533c6e2d9c31cc65bd
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-04-23 22:28:09 -0400

    Some work on partitioning (non-)beginner issues

--- Main.hs
+++ Main.hs
@@ -5,59 +5,99 @@
 import Control.Lens
 import Data.Aeson
 import Data.Aeson.Lens
-import IO (print)
+import IO (putStrLn)
 import Network.HTTP.Simple
 import Text (encodeUtf8)
-import Vector (Vector)
 
 import qualified ByteString
-import qualified ByteString.Partial as ByteString
 import qualified Data.ByteString.Char8 as Char8
 import qualified ListT as ListT
 import qualified Set as Set
+import qualified Text
 
 main :: IO ()
 main = do
-  repos :: Vector Value <-
-    githubGetHaskellRepos
+  beginnerLabels :: Set Text <-
+    (do
+      bytes :: Text <-
+        Text.readFile "etc/beginner-labels.txt"
+      pure (Set.fromList (Text.lines bytes)))
+    <|> pure mempty
 
-  let fullNames :: [Text]
-      fullNames =
-        take 1 (repos ^.. traverse . key "full_name" . _String)
+  notBeginnerLabels :: Set Text <-
+    (do
+      bytes :: Text <-
+        Text.readFile "etc/not-beginner-labels.txt"
+      pure (Set.fromList (Text.lines bytes)))
+    <|> pure mempty
 
-  for_ fullNames $ \name -> do
-    githubGetIssues name
-      & ListT.foldM
-          (\() issue -> do
-            let labels :: Set Text
-                labels =
-                  issue ^. key "labels"
-                         . values
-                         . key "name"
-                         . _String
-                         . to Set.singleton
-            print labels)
-          (pure ())
-          pure
+  assert (null (Set.intersection beginnerLabels notBeginnerLabels)) (pure ())
 
-beginnerLabels :: Set Text
-beginnerLabels =
-  Set.fromList
-    [ "" ]
+  ListT.foldM
+    (\acc issue -> do
+      let labels :: Set Text
+          labels =
+            issue ^. key "labels"
+                   . values
+                   . key "name"
+                   . _String
+                   . to Set.singleton
 
-githubGetHaskellRepos :: IO (Vector Value)
+      -- Print issue if it contains at least one beginner label
+      unless (null (Set.intersection labels beginnerLabels)) $ do
+        let title :: Text
+            title =
+              issue ^?! key "title" . _String
+
+        let url :: Text
+            url =
+              issue ^?! key "url" . _String
+
+        Text.putStrLn (title <> " " <> url)
+
+      -- Accumulate unknown labels (to be categorized)
+      let unknownLabels :: Set Text
+          unknownLabels =
+            labels
+              Set.\\ beginnerLabels
+              Set.\\ notBeginnerLabels
+
+      pure (acc <> unknownLabels))
+    (pure mempty)
+    (\unknown ->
+      putStrLn ("[Unknown labels] " <> show (Set.toList unknown)))
+    (do
+      repo :: Value <-
+        ListT.take 1 githubGetHaskellRepos
+      githubGetIssues (repo ^?! key "full_name" . _String))
+
+githubGetHaskellRepos :: ListT IO Value
 githubGetHaskellRepos = do
-  request :: Request <-
-    parseRequest "https://api.github.com/search/repositories?q=language:haskell"
+  request0 :: Request <-
+    liftIO
+      (parseRequest
+        "https://api.github.com/search/repositories?q=language:haskell")
 
-  response :: Response ByteString <-
-    httpBS (request & addRequestHeader "User-Agent" "Haskell")
+  response0 :: Response ByteString <-
+    httpBS (request0 & addRequestHeader "User-Agent" "Haskell")
 
-  pure (getResponseBody response ^?! key "items" . _Array)
+  let loop :: Response ByteString -> ListT IO Value
+      loop response =
+        headPage <|> tailPages
+       where
+        headPage :: ListT IO Value
+        headPage =
+          ListT.select (getResponseBody response ^?! key "items" . _Array)
+
+        tailPages :: ListT IO Value
+        tailPages =
+          empty
+
+  loop response0
 
 githubGetIssues :: Text -> ListT IO Value
 githubGetIssues name = do
-  request :: Request <-
+  request0 :: Request <-
     liftIO
       (parseRequest
         (Char8.unpack
@@ -66,7 +106,7 @@ githubGetIssues name = do
             <> "/issues?per_page=100")))
 
   response0 :: Response Value <-
-    liftIO (httpJSON (request & addRequestHeader "User-Agent" "Haskell"))
+    liftIO (httpJSON (request0 & addRequestHeader "User-Agent" "Haskell"))
 
   let loop :: Response Value -> ListT IO Value
       loop response =
@@ -78,31 +118,35 @@ githubGetIssues name = do
 
         tailPages :: ListT IO Value
         tailPages = do
-          Just links <-
-            pure (lookup "Link" (getResponseHeaders response))
-
-          let f :: [ByteString] -> Maybe ByteString
-              f = \case
-                [link, " rel=\"next\""] ->
-                  link
-                    & Char8.dropWhile (/= '<')
-                    & ByteString.drop 1
-                    & ByteString.init
-                    & Just
-                _ ->
-                  Nothing
-
-          next :: ByteString <-
-            Char8.split ',' links
-              & mapMaybe (f . Char8.split ';')
-              & ListT.select
-
-          liftIO (print next)
-
           request :: Request <-
-            liftIO (parseRequest (Char8.unpack next))
-
+            maybe empty pure (getResponseNext response)
           liftIO (httpJSON (request & addRequestHeader "User-Agent" "Haskell"))
             >>= loop
 
   loop response0
+
+getResponseNext :: Response a -> Maybe Request
+getResponseNext response = do
+  bytes :: ByteString <-
+    lookup "Link" (getResponseHeaders response)
+
+  let links :: [ByteString]
+      links =
+        Char8.split ',' bytes
+
+  url:_ :: [ByteString] <-
+    pure (mapMaybe stripNext links)
+
+  parseRequest (Char8.unpack url)
+
+ where
+  stripNext :: ByteString -> Maybe ByteString
+  stripNext bytes = do
+    prefix :: ByteString <-
+      ByteString.stripSuffix "rel=\"next\"" bytes
+
+    prefix
+      & Char8.dropWhile (/= '<')
+      & ByteString.drop 1
+      & Char8.takeWhile (/= '>')
+      & Just
